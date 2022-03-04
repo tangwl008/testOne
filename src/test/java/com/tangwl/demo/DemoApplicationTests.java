829d9829bd5ec44347d4e167b3220a377ef42613
twl <111111>
2022-03-04 11:38:33 +0800

    测试

---
+++ src/test/java/com/tangwl/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangwl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
